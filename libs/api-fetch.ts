c77da950e76c1af4cf139b1c7c091e7f6aca1d3e
Parth Prajapati <parthprajapti2203@gmail.com>
2026-03-06 10:23:33 +0530

    handle api fetch logic that retuns null velue if anything error accurs but the API interface can't recive null values

--- libs/api-fetch.ts
+++ libs/api-fetch.ts
@@ -1,24 +1,20 @@
 import { Api, ApiResponse } from "@/types/apis";
 
-export async function fetchApis(apiUrl: string): Promise<Api[] | null> {
+export async function fetchApis(apiUrl: string): Promise<Api[]> {
   try {
     const res: Response = await fetch(apiUrl, {
       next: { revalidate: 300 },
     });
 
-    if (res.status === 404) {
-      return null;
-    }
-
     if (!res.ok) {
-      const errorData = await res.json();
-
-      throw new Error("Failed to fetch news", errorData);
+      throw new Error(`Failed to fetch news: ${res.status}`);
     }
 
     const data: ApiResponse = await res.json();
+
     return data.articles ?? [];
   } catch (error) {
-    throw new Error("Failed to fetch news");
+    console.error("Fetch API Error:", error);
+    return [];
   }
 }
